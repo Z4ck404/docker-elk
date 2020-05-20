39c1b5c3a9be19b01233586c0d9a2c57a69bebd4
Zakaria EL BAZI <zakaria.elbazi@exceleratesystems.net>
2020-05-20 13:58:43 +0200

    some edits in the sg config
    - using demo pem certificate
    - adding es-hadoop for tutorial purpose
    - disabeling logstash

--- elasticsearch/bin/init_sg.sh
+++ elasticsearch/bin/init_sg.sh
@@ -2,7 +2,8 @@
 
 plugins/search-guard-7/tools/sgadmin.sh \
 	-cd config/sg/ \
-	-ts config/sg/truststore.jks \
-	-ks config/sg/kirk-keystore.jks \
+	-key config/sg/certificates/kirk-key.pem \
+	-cert config/sg/certificates/kirk.pem \
+	-cacert config/sg/certificates/root-ca.pem\
 	-nhnv \
 	-icl
